947b94c2ef507424a9040e1508e4d00b20cbf434
ratul90-git <ratulde2022@gmail.com>
2025-02-19 22:09:31 +0530

    Removing bind mount and adding volume mount, removing pre-config setup for cloudbeaver

--- DATABASE/db-controller.sh
+++ DATABASE/db-controller.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set default values
-POSTGRES_USER="dreamer"
-POSTGRES_PASSWORD="Beginner@2025"
 POSTGRES_DB="matrix_db"
 SCHEMA_NAME="matrix_schema"
+POSTGRES_USER="dreamer"
+POSTGRES_PASSWORD="Beginner@2025"
 POSTGRES_PORT="6432"
 CLOUD_BEAVER_PORT="9080"
 
@@ -21,24 +21,24 @@ case "$ACTION" in
 esac
 
 # Export the environment variables
-export POSTGRES_USER
-export POSTGRES_PASSWORD
 export POSTGRES_DB
 export SCHEMA_NAME
+export POSTGRES_USER
+export POSTGRES_PASSWORD
 export POSTGRES_PORT
 export CLOUD_BEAVER_PORT
 
-# Substitute environment variables in the init_db.sql script
-sed -e "s/\${SCHEMA_NAME}/${SCHEMA_NAME}/g" \
+# Substitute environment variables in the .init_db_template.sql script
+sed -e "s/\${POSTGRES_DB}/${POSTGRES_DB}/g" \
+	-e "s/\${SCHEMA_NAME}/${SCHEMA_NAME}/g" \
     -e "s/\${POSTGRES_USER}/${POSTGRES_USER}/g" \
-    -e "s/\${POSTGRES_DB}/${POSTGRES_DB}/g" \
-    ./init_db_template.sql > ./init_db.sql
-
+    ./database-config/.init_db_template.sql > ./database-config/init_db.sql
+    
 # Perform action-specific commands
 if [ "$ACTION" == "start" ]; then
-  docker-compose -f ./database.yaml up -d
+  docker-compose -f ./db-compose.yaml up -d
 elif [ "$ACTION" == "stop" ]; then
-  docker-compose -f ./database.yaml down
+  docker-compose -f ./db-compose.yaml down
 fi
 
 # Check the exit code of docker-compose
